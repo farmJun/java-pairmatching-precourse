5d315257efe7f44ba5e1cef002322eedee54e629
박준영 <wnsdud6969@naver.com>
2023-12-14 15:50:39 +0900

    feat(FileReader): 크루들의 이름을 파일로부터 읽는 기능을 구현하다

---
+++ src/main/java/pairmatching/domain/FileReader.java
@@ -0,0 +1,33 @@
+package pairmatching.domain;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import pairmatching.domain.informtaion.Course;
+
+public class FileReader {
+
+    private FileReader() {
+
+    }
+
+    public static List<String> getCrewNames(Course course) {
+        try {
+            File file = new File(course.getPath());
+            java.io.FileReader fileReader = new java.io.FileReader(file);
+            BufferedReader bufReader = new BufferedReader(fileReader);
+
+            List<String> crews = new ArrayList<>();
+
+            String crewName = "";
+            while ((crewName = bufReader.readLine()) != null) {
+                crews.add(crewName);
+            }
+            return crews;
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
