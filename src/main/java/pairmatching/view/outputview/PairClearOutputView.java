2c7b985bad1068830524e13e65254d28ed245247
박준영 <wnsdud6969@naver.com>
2023-12-14 16:51:38 +0900

    feat(PairClearOutputView): 페어가 초기화 됐음을 출력하다

---
+++ src/main/java/pairmatching/view/outputview/PairClearOutputView.java
@@ -0,0 +1,13 @@
+package pairmatching.view.outputview;
+
+public class PairClearOutputView {
+
+    private PairClearOutputView() {
+
+    }
+
+    public static void printPairClear() {
+        System.out.println("초기화 되었습니다.");
+        System.out.println();
+    }
+}
