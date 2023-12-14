f9f962f8f2e824748afe7e346489c1069329faec
박준영 <wnsdud6969@naver.com>
2023-12-14 17:23:24 +0900

    feat(PairMatchingInputView): 페어 재매칭 여부를 입력받다

--- src/main/java/pairmatching/view/inputview/PairMatchingInputView.java
+++ src/main/java/pairmatching/view/inputview/PairMatchingInputView.java
@@ -26,4 +26,13 @@ public class PairMatchingInputView {
         String input = Console.readLine();
         return Arrays.asList(input.split(", "));
     }
+
+
+    public static String readRematching() {
+        System.out.println(" 매칭 정보가 있습니다.다시 매칭하시겠습니까?\n"
+            + "네 | 아니오");
+
+        return Console.readLine();
+    }
+
 }
